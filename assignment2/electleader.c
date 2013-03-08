a684b1fb6c2839b3e44857bddd9be1c2afa2a8c6
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2013-03-07 23:03:16 -0800

    Yuck.

--- assignment2/electleader.c
+++ assignment2/electleader.c
@@ -14,7 +14,57 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    printf("I am %d of %d\n", rank, size);
+    int id = ((rank + 1) * pnum) % size;
+    pack_elec(id, 15, 240);
+
+    /* Send message left and right. */
+    /*
+    char *right_msg = "Sent from right\n";
+    int right_len   = strlen(right_msg);
+    int right       = (rank + 1) % size;
+
+    char buffer[MSG_LEN];
+    MPI_Status status;
+
+    if(rank % 2)
+    {
+        MPI_Send(right_msg,
+            right_len,
+            MPI_CHAR,
+            right,
+            MPI_TAG,
+            MPI_COMM_WORLD);
+
+        MPI_Recv(buffer,
+            MSG_LEN,
+            MPI_CHAR,
+            MPI_ANY_SOURCE,
+            MPI_TAG,
+            MPI_COMM_WORLD,
+            &status); 
+
+        printf("Received: %s\n", buffer);
+    }
+    else
+    {
+        MPI_Recv(buffer,
+            MSG_LEN,
+            MPI_CHAR,
+            MPI_ANY_SOURCE,
+            MPI_TAG,
+            MPI_COMM_WORLD,
+            &status); 
+
+        printf("Received: %s\n", buffer);
+
+        MPI_Send(right_msg,
+            right_len,
+            MPI_CHAR,
+            right,
+            MPI_TAG,
+            MPI_COMM_WORLD);
+    }
+    */
 
     /* Exit cleanly. */
     MPI_Finalize();
@@ -45,4 +95,79 @@ int extract_pnum(char *pnum_arg)
 {
     int pnum = atoi(pnum_arg);
     return pnum;
+}
+
+
+/*
+ * Package an election message.
+ */
+char *pack_elec(int id, int k, int d)
+{
+    int id_size = num_size(id);
+    int k_size  = num_size(k);
+    int d_size  = num_size(d);
+
+    int total_length = strlen("elec...") + id_size + k_size + d_size;
+    char *result = malloc(sizeof(char) * total_length);
+
+    result[0] = 'e';
+    result[1] = 'l';
+    result[2] = 'e';
+    result[3] = 'c';
+    result[4] = '.';
+
+    int location = strlen("elec.");
+
+    if(id == 0)
+    {
+        result[location++] = '0';
+    }
+    else
+    {
+        while(id > 0)
+        {
+            int digit = id % 10;
+            result[location++] = '0' + digit;
+            id /= 10;
+        }
+    }
+
+    result[location++] = '.';
+
+    while(k > 0)
+    {
+        int digit = k % 10;
+        result[location++] = '0' + digit;
+        k /= 10;
+    }
+
+    result[location++] = '.';
+
+    while(d > 0)
+    {
+        int digit = d % 10;
+        result[location++] = '0' + digit;
+        d /= 10;
+    }
+
+    printf("[DEBUG] msg = %s\n", result);
+
+    return result;
+}
+
+
+/*
+ * Determine the number of digits in an int.
+ */
+int num_size(int num)
+{
+    int size = 0;
+
+    while(num > 0)
+    {
+        num /= 10;
+        size++;
+    }
+
+    return size;
 }
--- assignment2/electleader.h
+++ assignment2/electleader.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mpi.h"
+
+#define MPI_TAG 1234
+#define MSG_LEN 64
 
 void check_args(int num_args);
 int extract_pnum(char *pnum_arg);
+char *pack_elec(int id, int k, int d);
+int num_size(int num);
+
