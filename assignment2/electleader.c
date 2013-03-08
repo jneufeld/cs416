99dd9047430e0625d96808ab09faf3b787e5ed0e
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2013-03-08 10:28:45 -0800

    Pack and unpack election messages.

--- assignment2/electleader.c
+++ assignment2/electleader.c
@@ -15,7 +15,11 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int id = ((rank + 1) * pnum) % size;
-    pack_elec(id, 15, 240);
+    char *send_msg = pack_elec(id, 0, 1);
+    printf("[PROC-%d] send_msg=%s\n", rank, send_msg);
+    printf("[PROC-%d] unpack_id=%d\n", rank, unpack_elec_id(send_msg));
+    printf("[PROC-%d] unpack_k=%d\n", rank, unpack_elec_k(send_msg));
+    printf("[PROC-%d] unpack_d=%d\n", rank, unpack_elec_d(send_msg));
 
     /* Send message left and right. */
     /*
@@ -108,7 +112,7 @@ char *pack_elec(int id, int k, int d)
     int d_size  = num_size(d);
 
     int total_length = strlen("elec...") + id_size + k_size + d_size;
-    char *result = malloc(sizeof(char) * total_length);
+    char *result = malloc((sizeof(char) * total_length) + 1);
 
     result[0] = 'e';
     result[1] = 'l';
@@ -117,6 +121,7 @@ char *pack_elec(int id, int k, int d)
     result[4] = '.';
 
     int location = strlen("elec.");
+    int pos = location + id_size - 1;
 
     if(id == 0)
     {
@@ -127,32 +132,133 @@ char *pack_elec(int id, int k, int d)
         while(id > 0)
         {
             int digit = id % 10;
-            result[location++] = '0' + digit;
+            result[pos] = '0' + digit;
             id /= 10;
+            location++;
+            pos--;
         }
     }
 
     result[location++] = '.';
 
-    while(k > 0)
+    pos = location + k_size - 1;
+    if(k == 0)
     {
-        int digit = k % 10;
-        result[location++] = '0' + digit;
-        k /= 10;
+        result[location++] = '0';
+    }
+    else
+    {
+        while(k > 0)
+        {
+            int digit = k % 10;
+            result[pos] = '0' + digit;
+            k /= 10;
+            location++;
+            pos--;
+        }
     }
 
     result[location++] = '.';
 
-    while(d > 0)
+    pos = location + d_size - 1;
+    if(d == 0)
     {
-        int digit = d % 10;
-        result[location++] = '0' + digit;
-        d /= 10;
+        result[location++] = '0';
+    }
+    else
+    {
+        while(d > 0)
+        {
+            int digit = d % 10;
+            result[pos] = '0' + digit;
+            d /= 10;
+            location++;
+            pos--;
+        }
     }
 
-    printf("[DEBUG] msg = %s\n", result);
+    result[location] = '\0';
 
     return result;
+}
+
+
+/*
+ * Unpack and return a number from an election message.
+ */
+int unpack_elec_num(char *msg, int start)
+{
+    int result = 0;
+    int end = start + 1;
+
+    while(msg[end] != '.' && msg[end] != '\0')
+    {
+        end++;
+    }
+
+    int len = end - start;
+
+    while(msg[start] != '.' && msg[start] != '\0')
+    {
+        int digit = msg[start] - '0';
+        digit = digit * pow(10, len - 1);
+        result += digit;
+        start++;
+        len--;
+    }
+
+    return result;
+}
+
+
+/*
+ * Unpack and return the ID from an election message.
+ */
+int unpack_elec_id(char *msg)
+{
+    int id_start = strlen("elec.");
+    return unpack_elec_num(msg, id_start);
+}
+
+
+/*
+ * Unpack and return the k value from an election message.
+ */
+int unpack_elec_k(char *msg)
+{
+    int id_start = strlen("elec.");
+    int k_start  = id_start + 1;
+
+    while(msg[k_start] != '.')
+    {
+        k_start++;
+    }
+
+    return unpack_elec_num(msg, k_start + 1);
+}
+
+
+/*
+ * Unpack and return the d value from an election message.
+ */
+int unpack_elec_d(char *msg)
+{
+    int id_start = strlen("elec.");
+    int k_start  = id_start + 1;
+
+    while(msg[k_start] != '.')
+    {
+        k_start++;
+    }
+
+    int d_start = k_start + 1;
+
+    while(msg[d_start] != '.')
+    {
+        d_start++;
+    }
+
+    return unpack_elec_num(msg, d_start + 1);
 }
 
 
@@ -163,10 +269,17 @@ int num_size(int num)
 {
     int size = 0;
 
-    while(num > 0)
+    if(num == 0)
     {
-        num /= 10;
-        size++;
+        size = 1;
+    }
+    else
+    {
+        while(num > 0)
+        {
+            num /= 10;
+            size++;
+        }
     }
 
     return size;
--- assignment2/electleader.h
+++ assignment2/electleader.h
@@ -1,13 +1,32 @@
+/* -----------------------------------------------------------------------------
+ * Includes.
+ * -------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "mpi.h"
 
+/* -----------------------------------------------------------------------------
+ * Definitions.
+ * -------------------------------------------------------------------------- */
 #define MPI_TAG 1234
 #define MSG_LEN 64
 
+/* -----------------------------------------------------------------------------
+ * Function prototypes for general functions.
+ * -------------------------------------------------------------------------- */
 void check_args(int num_args);
 int extract_pnum(char *pnum_arg);
+
+/* -----------------------------------------------------------------------------
+ * Function prototypes for packing/unpacking messages.
+ * -------------------------------------------------------------------------- */
 char *pack_elec(int id, int k, int d);
 int num_size(int num);
+int unpack_elec_id(char *msg);
+int unpack_elec_num(char *msg, int start);
+int unpack_elec_id(char *msg);
+int unpack_elec_k(char *msg);
+int unpack_elec_d(char *msg);
 
